94cafceb368503b872cff5e27b768e3bef0c4564
shomix <100691266+shomix@users.noreply.github.com>
2023-06-30 10:40:07 +0200

    Branch 113 (#1)

---
+++ create-branches.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Specify the number of branches you want to create
+num_branches=115
+
+# Loop to create branches
+for ((i=1; i<=num_branches; i++))
+do
+    branch_name="branch-$i"
+    
+    # Checkout to the new branch
+    git checkout -b $branch_name
+    
+    # Make changes to the index.html
+    echo $branch_name >> "index.html"
+
+    # Commit and push the changes
+    git add .
+    git commit -m $branch_name
+    git push -u origin $branch_name
+
+done
