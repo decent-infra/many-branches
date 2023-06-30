bb422e1be8abaafbe15093f71882241eebc477e8
shomix <shomixpetrovic@gmail.com>
2023-06-30 10:38:10 +0200

    branch-54

--- create-branches.sh
+++ create-branches.sh
@@ -7,10 +7,14 @@ num_branches=115
 for ((i=1; i<=num_branches; i++))
 do
     branch_name="branch-$i"
+    
+    # Checkout to the new branch
     git checkout -b $branch_name
-
+    
+    # Make changes to the index.html
     echo $branch_name >> "index.html"
-    # # Create a new branch
+
+    # Commit and push the changes
     git add .
     git commit -m $branch_name
     git push -u origin $branch_name
